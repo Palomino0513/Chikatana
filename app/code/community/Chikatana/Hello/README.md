5a14712d0c9e1431b28582935360f27076aa86c2
Palomino <palomino@pengostores.com>
2016-09-12 13:21:30 -0500

    Hello prueba v1

---
+++ app/code/community/Chikatana/Hello/README.md
@@ -0,0 +1,1 @@
+# Chikatana_Hello
--- app/code/community/Chikatana/Hello/controllers/HelloController.php
+++ app/code/community/Chikatana/Hello/controllers/HelloController.php
@@ -6,4 +6,12 @@ class Chikatana_Hello_HelloController extends Mage_Core_Controller_Front_Action
         $this->loadLayout();
         $this->renderLayout();
     }
+
+    public function infoAction() {
+        $response = {
+            name = 'Palomino'
+        };
+        $this->getResponse()->clearHeaders()->setHeader('Content-type', 'application/json', true);
+        $this->getResponse()->setBody(json_encode($response));
+    }
 }
